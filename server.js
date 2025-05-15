87f1a37ed67ac784685d1787790836554a1ca8a5
aichaib <2712985@collegelacite.ca>
2025-05-14 21:35:19 -0400

    Ajout des fichier nodes modules et env dans le gitignore

--- server.js
+++ server.js
@@ -5,20 +5,53 @@ import "dotenv/config";
 import routerExterne from "./routes.js";
 
 // Importation des fichiers et librairies
+import { engine } from "express-handlebars";
 import express, { json } from "express";
 import helmet from "helmet";
 import compression from "compression";
 import cors from "cors";
 import cspOption from "./csp-options.js";
 
+// Importation de la session
+import session from "express-session";
+//importation de memorystore
+import memorystore from "memorystore";
+// Importation de passport
+import passport from "passport";
+
+import "./authentification.js";
+
 // Crréation du serveur express
 const app = express();
+
+//initialisation de la memoire de session
+const MemoryStore = memorystore(session);
+
+app.engine("handlebars", engine()); //Pour indiquer a express que l'on utilise handlebars
+app.set("view engine", "handlebars"); //Pour indiquer le rendu des vues
+app.set("views", "./views"); //Pour indiquer le dossier des vues
 
 // Ajout de middlewares
 app.use(helmet(cspOption));
 app.use(compression());
 app.use(cors());
 app.use(json());
+
+//Middeleware pour gerer les sessions
+app.use(
+    session({
+        cookie: { maxAge: 3600000 },
+        name: process.env.npm_package_name,
+        store: new MemoryStore({ checkPeriod: 3600000 }),
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET,
+    })
+);
+
+//Middleware pour gerer passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Middeleware integre a express pour gerer la partie static du serveur
 //le dossier 'public' est la partie statique de notre serveur
@@ -29,8 +62,8 @@ app.use(routerExterne);
 
 // Renvoyer une erreur 404 pour les routes non définies
 app.use((request, response) => {
- // Renvoyer simplement une chaîne de caractère indiquant que la page n'existe pas
- response.status(404).send(`${request.originalUrl} Route introuvable.`);
+    // Renvoyer simplement une chaîne de caractère indiquant que la page n'existe pas
+    response.status(404).send(`${request.originalUrl} Route introuvable.`);
 });
 
 //Démarrage du serveur
